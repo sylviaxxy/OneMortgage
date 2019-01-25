4a6f5028f47581d6da2578b0d0c253d09c8bda19
sylviaxxy <xuxinyusylvia@outlook.com>
2019-01-25 10:23:53 -0500

    add freddie_data_prepare.py

--- src/freddie_data_prepare.py
+++ src/freddie_data_prepare.py
@@ -1,23 +1,73 @@
 # PySpark code to load, merge, clean freddie data
-import zipfile
+
+# unzip and upload all text files back to s3
+import findspark
+import os
 import io
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from pyspark import SparkConf
+from pyspark import SparkFiles
+#sc = SparkContext()
+findspark.init()
 
-def zip_extract(x):
-    in_memory_data = io.BytesIO(x[1])
-    file_obj = zipfile.ZipFile(in_memory_data, "r")
-    files = [i for i in file_obj.namelist()]
-    return dict(zip(files, [file_obj.open(file).read() for file in files]))
+import boto3
+import botocore
+import zipfile
 
+s3 = boto3.resource('s3')
 
-zips = sc.binaryFiles("s3:/Testing/*.zip") #arn:aws:s3:::onemortgage
-files_data = zips.map(zip_extract).collect()
+#BUCKET_NAME = 'onemortgage'
+my_bucket = s3.Bucket('onemortgage')
 
-spark = SparkSession.builder
-                        .master("local")
-                        .appName("app name")
-                        .config("spark.some.config.option", true).getOrCreate()
+def download_zip(filename):
 
-df = spark.read.parquet("s3://path/to/parquet/file.parquet")
+    # download zipped files
+
+    s3 = boto3.resource('s3')
+    BUCKET_NAME = 'onemortgage'
+    foldername = 'freddie/'
+
+    KEY = foldername+filename
+
+    try:
+        s3.Bucket(BUCKET_NAME).download_file(KEY, filename)
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
+
+def zip_unzip(filename):
+    # unzip the file
+    zip = zipfile.ZipFile(filename)
+    zip.extractall()
+    zip_namelist = zipfile.ZipFile(filename).namelist()
+    return zip_namelist
+
+def textfile_upload_remove(filename,zip_namelist):
+    # upload the unzipped txt files back to s3 and remove the zip file in localhost
+    foldername = 'freddie/'
+    for txtfile_name in zip_namelist:
+        s3.Bucket('onemortgage').upload_file(txtfile_name, foldername+txtfile_name)
+        os.remove(txtfile_name)
+    os.remove(filename)
+
+def print_ziplist():
+    zip_list = []
+    for obj in my_bucket.objects.filter(Prefix='freddie/historical'):
+        #print('{0}:{1}'.format(my_bucket.name,obj.key))
+        zip_list.append(str(obj.key).replace('freddie/', ""))
+    return zip_list
+
+def unzip_all_rawzip():
+    s3 = boto3.resource('s3')
+    my_bucket = s3.Bucket('onemortgage')
+    zip_list = print_ziplist()
+    for zip_file in zip_list:
+        download_zip(zip_file)
+        zip_namelist = zip_unzip(zip_file)
+        textfile_upload_remove(zip_file,zip_namelist)
+    print('upzip job complete!')
+
+unzip_all_rawzip()
