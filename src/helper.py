8d55296f80c183b2c92f823e90cea66778d7fdbc
sylviaxxy <xuxinyusylvia@outlook.com>
2019-02-04 01:28:53 -0500

    edit helper.py

--- src/helper.py
+++ src/helper.py
@@ -4,6 +4,13 @@ import json
 import re
 import subprocess
 
+def read_text_file(url_pattern,real_colnames):
+    data = sc.textFile(url_pattern)
+    data_1 = data.map(lambda x: x.split('|').map(lambda x: [i.encode('utf-8') for i in x])
+    df = sqlContext.createDataFrame(data_1)
+    for c , n in zip(df.columns, real_colnames):
+        df = df.withColumnRenamed(c , n)
+    return df
 
 def parse_config(configfile):
     """
