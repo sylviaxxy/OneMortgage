189989141d85a6fe2ead9c0f5977fdc96bac5974
sylviaxxy <xuxinyusylvia@outlook.com>
2019-02-04 10:28:25 -0500

    fix bugs

--- src/helper.py
+++ src/helper.py
@@ -21,7 +21,7 @@ sqlContext = pyspark.SQLContext(sc)
 
 def read_text_file(url_pattern,real_colnames):
     data = sc.textFile(url_pattern)
-    data_1 = data.map(lambda x: x.split('|').map(lambda x: [i.encode('utf-8') for i in x])
+    data_1 = data.map(lambda x: x.split('|')).map(lambda x: [i.encode('utf-8') for i in x])
     df = sqlContext.createDataFrame(data_1)
     for c , n in zip(df.columns, real_colnames):
         df = df.withColumnRenamed(c , n)
